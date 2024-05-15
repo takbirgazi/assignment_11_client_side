4ff68405d9fc53ed2005304b90b6d23f2b78e1e5
takbirgazi <takbirgazibd@gmail.com>
2024-05-15 09:45:24 +0600

    Add Room Deatiels

--- src/pages/Rooms/RoomDeatiels.jsx
+++ src/pages/Rooms/RoomDeatiels.jsx
@@ -1,9 +1,28 @@
+import { useLoaderData } from "react-router-dom";
 
 
 const RoomDeatiels = () => {
+    const room = useLoaderData();
+    console.log(room)
     return (
-        <div>
-            this is room deatiels
+        <div className="my-5 w-10/12 mx-auto">
+            <div className="card card-compact w-full bg-base-100 shadow-xl">
+                <figure><img src={room.image} alt="Shoes" /></figure>
+                <div className="card-body">
+                    <p>{room.description}</p>
+                    <div className="card-actions flex flex-col">
+                        <button><span className="font-bold text-xl">${room.price_per_night}</span> per night</button>
+                        <p>Room Size: {room.room_size}</p>
+                        <div className="flex items-center justify-between w-full">
+                            <button className="py-1 px-2 border rounded bg-blue-500 text-white">{room.availability}</button>
+                            <h2 className="card-title"> 
+                                {room.special_offer}
+                                <div className="badge badge-secondary">Offer</div>
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
